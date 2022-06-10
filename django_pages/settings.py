e02ee820c4f9bbd201caca41af0559cda90fff85
Salman Farooq <salman.farooq3310@gmail.com>
2022-06-10 19:32:49 +0500

    message

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f27+&^dv(og0y*70tbvvawf!ytg&_2a!jt8m8ct(3ep(n%9q%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
